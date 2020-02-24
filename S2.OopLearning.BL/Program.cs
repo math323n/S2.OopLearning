61d71c15f4d5cc772bd37a35a5c37b29a82c65d0
sut <math323n@edu.campusvejle.dk>
2020-02-24 13:05:19 +0100

    Add project files.

---
+++ S2.OopLearning.BL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S2.OopLearning.BL
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
