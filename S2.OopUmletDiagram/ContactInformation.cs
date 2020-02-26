ae0ebb3ba1b7f7954f9cc19d6ae8062bafe9b4c8
sut <math323n@edu.campusvejle.dk>
2020-02-26 11:43:48 +0100

    Documentation for contact information

--- S2.OopUmletDiagram/ContactInformation.cs
+++ S2.OopUmletDiagram/ContactInformation.cs
@@ -4,6 +4,14 @@ using System.Text;
 
 namespace S2.OopUmletDiagram
 {
+    /// <summary>
+    ///reates a new <see cref="ContactInformation"/> with the provided <see cref="id"/>, <see cref="email"/>, <see cref="phoneNumber"/>
+    /// </summary>
+    /// <param name="id">The persons ID</param>
+    /// <param name="email">The persons firstname</param>
+    /// <param name="phoneNumber">The persons lastname</param>
+    ///<exception cref="ArgumentNullException"></exception>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
     public class ContactInformation
     {
         private int id;
@@ -17,11 +25,20 @@ namespace S2.OopUmletDiagram
             PhoneNumber = phoneNumber;
         }
 
+        /// <summary>
+        /// Gets or sets value of <see cref="id"/>
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public int Id
         {
             get => id;
             set
             {
+                (bool isValid, string errorMessage) validationResult = ValidateId(value);
+                if(!validationResult.isValid)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Id), validationResult.errorMessage);
+                }
                 if(value != id)
                 {
                     id = value;
@@ -29,10 +46,19 @@ namespace S2.OopUmletDiagram
             }
         }
 
+        /// <summary>
+        /// Gets or sets value of <see cref="email"/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
         public string Email        {
             get => email;
             set
             {
+                (bool isValid, string errorMessage) validationResult = ValidateEmail(value);
+                if(!validationResult.isValid)
+                {
+                    throw new ArgumentNullException(nameof(Email), validationResult.errorMessage);
+                }
                 if(value != email)
                 {
                     email = value;
@@ -40,11 +66,20 @@ namespace S2.OopUmletDiagram
             }
         }
 
+        /// <summary>
+        /// Gets or sets value of <see cref="phoneNumber"/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
         public string PhoneNumber
         {
             get => phoneNumber;
             set
             {
+                (bool isValid, string errorMessage) validationResult = ValidatePhoneNumber(value);
+                if(!validationResult.isValid)
+                {
+                    throw new ArgumentNullException(nameof(PhoneNumber), validationResult.errorMessage);
+                }
                 if(value != phoneNumber)
                 {
                     phoneNumber = value;
@@ -52,6 +87,11 @@ namespace S2.OopUmletDiagram
             }
         }
 
+        /// <summary>
+        /// Validates the ID of the Contact Information, must not be => 0
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public static (bool, string) ValidateId(int id)
         {
             if(id <= 0)
@@ -64,6 +104,11 @@ namespace S2.OopUmletDiagram
             }
         }
 
+        /// <summary>
+        /// Validates the email of the Contact Information, must not be NULL or WHITESPACE
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
         public static (bool, string) ValidateEmail(string email)
         {
             if(string.IsNullOrWhiteSpace(email))
@@ -76,6 +121,11 @@ namespace S2.OopUmletDiagram
             }
         }
 
+        /// <summary>
+        /// Validates the phoneNumber of the Contact Information, must not be NULL or WHITESPACE
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns></returns>
         public static (bool, string) ValidatePhoneNumber(string phone)
         {
             if(string.IsNullOrWhiteSpace(phone))
