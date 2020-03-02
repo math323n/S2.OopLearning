ee2fb3e4995f08c73a2d858da0f4a6f70af433d4
sut <math323n@edu.campusvejle.dk>
2020-03-02 13:48:07 +0100

    Mere du kian copy paste fra mig.

---
+++ S2.OopInheritance/Inheritance/VideoFileInfo.cs
@@ -0,0 +1,55 @@
+﻿using S2.OopLearning.BL.Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2.OopInheritance.Inheritance
+{
+    class VideoFileInfo : ImageFileInfo
+    {
+        private int duration;
+
+        public VideoFileInfo(int width, int height, string fileName, int duration, int fileSize, DateTime creationTime)
+            : base(width, height, fileName, fileSize, creationTime)
+        {
+            Duration = duration;
+        }
+
+        public int Duration
+        {
+            get
+            {
+                return duration;
+            }
+
+            set
+            {
+                (bool isValid, string errorMessage) validationResult = ValidateDuration(value);
+                if(!validationResult.isValid)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Duration), validationResult.errorMessage);
+                }
+                if(value != duration)
+                {
+                    duration = value;
+                }
+            }
+        }
+
+        public (bool, string) ValidateDuration(int minutes)
+        {
+            if(minutes <= 0)
+            {
+                
+                return (false, "Filmen må ikke være på 0 minutter eller derunder.");
+            }
+            else
+            {
+                return (true, String.Empty);
+            }
+        }
+
+        public override string ToString()
+        => $"Bredde: {Width}\nLængde: {Height}\nVideo: {fileName}\nMinutter: {duration}\nFilstørelse: {fileSize} MB\nOprettet: {creationTime.ToString("dd/MM/yyyy")}";
+    }
+}
--- S2.OopInheritance/Program.cs
+++ S2.OopInheritance/Program.cs
@@ -11,9 +11,10 @@ namespace S2.OopInheritance
             {
                 DocumentFileInfo document = new DocumentFileInfo("autistfil", 12, new DateTime(2004, 12, 02));
                 ImageFileInfo image = new ImageFileInfo(1920, 984, "autist", 23, new DateTime(1982,09,12));
-                Console.WriteLine(image);
-                
+  
+                VideoFileInfo video = new VideoFileInfo(1920, 1080, "film om autisme", 100, 28, new DateTime(1971, 09, 12));
 
+                Console.WriteLine(video);
             }
             catch(ArgumentOutOfRangeException e)
             {
