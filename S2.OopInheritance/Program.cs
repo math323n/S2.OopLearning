dde53abc94723a2a37a0b3723ca3007d50256a14
sut <math323n@edu.campusvejle.dk>
2020-03-02 14:42:12 +0100

    færdig

--- S2.OopInheritance/Program.cs
+++ S2.OopInheritance/Program.cs
@@ -10,8 +10,9 @@ namespace S2.OopInheritance
             try
             {
                 DocumentFileInfo document = new DocumentFileInfo("autistfil", 12, new DateTime(2004, 12, 02));
+
                 ImageFileInfo image = new ImageFileInfo(1920, 984, "autist", 23, new DateTime(1982,09,12));
-  
+
                 VideoFileInfo video = new VideoFileInfo(1920, 1080, "film om autisme", 100, 28, new DateTime(1971, 09, 12));
 
                 Console.WriteLine(video);
