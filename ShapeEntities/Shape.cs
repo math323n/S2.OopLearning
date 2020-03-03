e37bcce3d050086d61c7af46b33e4adf6f3e9e34
sut <math323n@edu.campusvejle.dk>
2020-03-03 10:48:19 +0100

    Started projekt

---
+++ ShapeEntities/Shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ShapeEntities
+{
+    public class Shape
+    {
+        private int x;
+        private int y;
+
+        public Shape()
+        {
+
+        }
+
+        protected int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        protected int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+    }
+}
---
+++ ShapeManualTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShapeManualTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
